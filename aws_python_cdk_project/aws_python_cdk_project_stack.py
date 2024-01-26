2f861792113202e20ff9f8b041c57ebb40bf91f9
Uday Singh <110370486+Matrixbravo@users.noreply.github.com>
2024-01-26 20:58:38 +0530

    Updated CDK and Python code

--- aws_python_cdk_project/aws_python_cdk_project_stack.py
+++ aws_python_cdk_project/aws_python_cdk_project_stack.py
@@ -1,24 +1,22 @@
 import os
 from aws_cdk import core
-from aws_cdk import aws_cloudformation as cfn
-from constructs import Construct
 
 class AwsPythonCdkProjectStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str,include_services: list, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, construct_id: str, include_services: list, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        #Read and deploy CloudFormation templates from the 'templates folder'
-        templates_folder = os.path(os.path.dirname(__file__), 'templates')
+        # Read and deploy CloudFormation templates from the 'templates' folder
+        templates_folder = os.path.join(os.path.dirname(__file__), 'templates')
         
         for service in include_services:
-            self.include_services_templates(templates_folder, service)
+            self.include_services_template(templates_folder, service)
             
     def include_services_template(self, templates_folder: str, service: str) -> None:
         service_template_path = os.path.join(templates_folder, f'{service.lower()}_template.yaml')
         
         if os.path.exists(service_template_path):
-            core.CfnInclude(self, f'{service.capitalize()}Stack',template=service_template_path)
+            core.CfnInclude(self, f'{service.capitalize()}Stack', template=service_template_path)
         else:
             print(f"Template for {service} not found at {service_template_path}")
             
